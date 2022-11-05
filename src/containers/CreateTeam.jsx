a41643924027a33e5800ba6da788355b38470407
geistnine <sgeiger5516@gmail.com>
2022-11-05 17:51:04 -0400

    added styles and classNames, adjusted createTeam form buttons
    Co-authored-by: Ahsan Ali <110138940+greyali@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>

--- src/containers/CreateTeam.jsx
+++ src/containers/CreateTeam.jsx
@@ -11,33 +11,35 @@ function CreateTeam() {
   return (
     <div className='create-team flex-column flex-center container-card'>
       <h1>Create Team</h1>
-      <form className='form' onSubmit={(e) => {
+      <form className='form flex-column' onSubmit={(e) => {
         e.preventDefault();
         console.log('Team Creation Submitted!')
         createTeam();
       }}>
         <div>
           <label for='name'>Team name:</label>
-          <input type='text'></input>
+          <input className="form-input-box" type='text'></input>
         </div>
         <div>
           <label for='member'>Member 1</label>
-          <input type='text'></input>
+          <input className="form-input-box" type='text'></input>
         </div>
         <div>
           <label for='member'>Member 2</label>
-          <input type='text'></input>
+          <input className="form-input-box" type='text'></input>
         </div>
         <div>
           <label for='member'>Member 3</label>
-          <input type='text'></input>
+          <input className="form-input-box" type='text'></input>
         </div>
         <div>
           <label for='member'>Member 4</label>
-          <input type='text'></input>
+          <input className="form-input-box" type='text'></input>
         </div>
-        <Link to='/'><button className='button'>Cancel</button></Link>
-        <input className='button' type='submit' value='Create'></input>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Link to='/'><button className='button'>Cancel</button></Link>
+          <input className='button align-self-end' type='submit' value='Create'></input>
+        </div>
       </form>
     </div>
   )
