93f5b4f660f9a64043a1ecf4e5960c352c98941f
greyali <grey.ali21@gmail.com>
2022-11-06 16:43:23 -0500

    Revised aspects of DB forms and worked on connecting the Login/Registration Forms to the backend.
    Co-authored-by: Ahsan Ali <110138940+greyali@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>

--- src/components/LoginButtons.jsx
+++ src/components/LoginButtons.jsx
@@ -4,6 +4,35 @@ import { Link, useNavigate } from 'react-router-dom';
 const LoginButtons = (props) => {
   // Navigate hook for login conditional
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    
+    // console.log('boolResp from handleClick ', boolResp);
+    // if(boolResp === true) navigate('/home')
+    // else {
+    //   console.log('else block in handleClick');
+    //   alert('Login failed')
+    // }
+
+    fetch('/db/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: props.username, password: props.password})
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('Login response: ', data.login_success);
+        if(data.login_success === true) return navigate('/home');
+      })
+      .catch(err => {
+        console.error('Error: ', err);
+        return alert('Login failed');
+      });
+
+  }
+  
   return (
     <div>
       <Link>
@@ -12,12 +41,12 @@ const LoginButtons = (props) => {
         </button>
       </Link>
 
-      <button onClick={() => {
-        // Conditional login or wrong
-        return props.loginAccount()
-          ? navigate('/home')
-          : alert('Wrong login information!');
-      }}>
+      <button onClick= { handleClick 
+        // // Conditional login or wrong
+        // return props.loginAccount()
+        //   ? navigate('/home')
+        //   : alert('Wrong login information!');
+      }>
         Login
       </button>
 
