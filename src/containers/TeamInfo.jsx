97c08afdfe6a480ccdace3063e5e756c3a2a7961
atu816 <alexandertu95@gmail.com>
2022-11-06 22:16:00 -0800

    Removed minor comments. Will continue cleaning for next commit
    Co-authored-by: Ahsan Ali <110138940+greyali@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>

--- src/containers/TeamInfo.jsx
+++ src/containers/TeamInfo.jsx
@@ -1,27 +1,13 @@
-/*
-  TeamInfo Page goals: 
-    [x]See Team info (TeamID, Team Members, List of activities)
-    [x]Display all activities from DB as components
-      [x]Alex:Alex set up state of page from info on the DB call
-      [x]Info is drilled in from specific team id reference in component above (TotalTeamDisplay)
-    []Create 'Add Activity' button
-      []Fetches from API for an activity based on group size (other info/preferences can be stretch)
-      []Adds activity to our DB into our specific team's activities
-      []Alex:Alex Resync DB to state with hooks
-*/
 import React, { useEffect, useState, useContext } from 'react';
 import { TeamsContext } from '../App.jsx';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 
 function TeamInfo(props) {
-  // useNavigate hook to redirect
   const navigate = useNavigate();
-  // Storing location information sent fom Link 
   const location = useLocation();
-  // Passing in total teams state from App.jsx (Arr of Objects)
   const totalTeamsArr = useContext(TeamsContext);
-  // Filter to find this specific team's info from totalTeams
+  
   const currTeam = totalTeamsArr.filter(obj => obj.teamName === location.state.teamName)
   console.log('totalTeamsArr', totalTeamsArr);
   console.log('currTeam', currTeam);
@@ -31,7 +17,6 @@ function TeamInfo(props) {
   const [teamInfo, setUpdateTeam] = React.useState(...currTeam);
   console.log('Current team info state:', teamInfo);
 
-  // Generate an activity.
   // Alex:Backend - Fetch activity from API and parse into following
   const getActivity = () => {
     // Test data to generate random activity (replace with API fetch)
@@ -58,9 +43,7 @@ function TeamInfo(props) {
     }
 
     // useState helper updates our App.jsx total state for this user
-    // (Arr of objects)
     props.sync(totalTeamsClone)
-    // useState to update our current page's data
     setUpdateTeam({ ...currTeamClone })
   }
 
