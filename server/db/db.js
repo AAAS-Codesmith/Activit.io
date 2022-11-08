25a7eef4643a6148b221971c9d4ba5e4b2e37445
John Wroge <wrogejohn@gmail.com>
2022-11-08 18:08:37 -0500

    Co-authored-by: James Howat <97859901+jbhowat@users.noreply.github.com> Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com> Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com> Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>

--- server/db/db.js
+++ server/db/db.js
@@ -1,30 +1,35 @@
 // Will solely be used to set up the database connection
 // Import path module
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 // Import mongoose
-const mongoose = require('mongoose');
-require('dotenv').config({path: path.resolve(__dirname, '../../process.env')});
+const mongoose = require("mongoose");
+require("dotenv").config({
+  path: path.resolve(__dirname, "../../process.env"),
+});
 
 // Import the database URI from the .env file
 const mongoDB = process.env.MONGOURI;
 
-console.log('\n');
-console.log('\u001b[1;36mmongoDB: ', mongoDB);
+console.log("\n");
+console.log("\u001b[1;36mmongoDB: ", mongoDB);
 
 // Set up mongoose connection
-mongoose.connect(mongoDB, {bufferCommands: false})
-  .then(() => console.log('\u001b[1;36m ---- Connected to MongoDB ----\n'))
-  .catch((err) => console.log('Error connecting to MongoDB: ', err));
-
-mongoose.connection.on('error', err => LogError(err));
+mongoose
+  .connect(mongoDB, { bufferCommands: false })
+  .then(() => console.log("\u001b[1;36m ---- Connected to MongoDB ----\n"))
+  .catch((err) => console.log("Error connecting to MongoDB: ", err));
 
 // Get the default connection
 const db = mongoose.connection;
 
+db.on("error", (err) => LogError(err));
+
 // Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, '\u001b[1;31m MongoDB connection error:'));
+db.on(
+  "error",
+  console.error.bind(console, "\u001b[1;31m MongoDB connection error:")
+);
 
 // Export db
 module.exports = db;
-
